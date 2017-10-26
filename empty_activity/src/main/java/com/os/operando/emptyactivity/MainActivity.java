e24d47bf726a96d203205b75a124e4d683f5b4eb
Franklin Yang <fyang016@ucr.edu>
2017-10-26 14:38:47 -0700

    added working cookie clicker

--- empty_activity/src/main/java/com/os/operando/emptyactivity/MainActivity.java
+++ empty_activity/src/main/java/com/os/operando/emptyactivity/MainActivity.java
@@ -2,12 +2,22 @@ package com.os.operando.emptyactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        count++;
+        TextView txt = (TextView) findViewById(R.id.text1);
+        txt.setText("You tapped " + count + " time(s).");
     }
 }
