e7c0fb47ef08771651a11ceed058de606c7a09d6
Kristen Jue <kjue001@ucr.edu>
2017-11-01 16:18:08 -0700

    Added create account (profile)

--- empty_activity/src/main/java/com/os/operando/emptyactivity/MainActivity.java
+++ empty_activity/src/main/java/com/os/operando/emptyactivity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.os.operando.emptyactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,5 +20,9 @@ public class MainActivity extends AppCompatActivity {
         count++;
         TextView txt = (TextView) findViewById(R.id.text1);
         txt.setText("You tapped " + count + " time(s).");
+    }
+    public void gotoca(View view)
+    {
+        startActivity(new Intent(getApplicationContext(),CreateAccount.class));
     }
 }
