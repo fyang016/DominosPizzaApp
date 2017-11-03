d7da951c5748f09a3f1d4a300df423645e147092
Franklin Yang <fyang016@ucr.edu>
2017-11-03 14:23:26 -0700

    added a few buttons

--- empty_activity/src/main/java/com/os/operando/emptyactivity/MainActivity.java
+++ empty_activity/src/main/java/com/os/operando/emptyactivity/MainActivity.java
@@ -6,10 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+    ImageButton androidImageButton;
     int count = 0;
 
     @Override
@@ -17,6 +20,17 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        androidImageButton = (ImageButton) findViewById(R.id.image_button_android);
+
+        androidImageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                count++;
+                Toast.makeText(MainActivity.this, "It works! " + count, Toast.LENGTH_LONG).show();
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, OrderPizza.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
     }
 
     public void onClick(View view) {
