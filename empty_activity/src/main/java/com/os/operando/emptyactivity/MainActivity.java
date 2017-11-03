80d2c9f73a731db75e3a0a4117f25aa942d4edf2
Franklin Yang <fyang016@ucr.edu>
2017-11-03 03:31:40 -0700

    added splash logo and animation

--- empty_activity/src/main/java/com/os/operando/emptyactivity/MainActivity.java
+++ empty_activity/src/main/java/com/os/operando/emptyactivity/MainActivity.java
@@ -1,18 +1,21 @@
 package com.os.operando.emptyactivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 
