ed25a440679670642f426597a79a70059100a45e
Kristen Jue <kjue001@ucr.edu>
2017-11-03 18:07:04 -0700

    Database working

--- empty_activity/src/main/java/com/os/operando/emptyactivity/MainActivity.java
+++ empty_activity/src/main/java/com/os/operando/emptyactivity/MainActivity.java
@@ -6,14 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    String txt = "";
     int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.editTextE);
+        DatabaseHelp dbs = new DatabaseHelp(this);
+
+        /*dbs.addUsers(new LoginInfo("kjue001", "1235"));
+
+        List<LoginInfo> logininfo = dbs.allUsers();
+
+        for(LoginInfo l: logininfo)
+        {
+            String log = "Email " + l.getEmail() + "Password " + l.getPassword() + "\n";
+            txt = txt +log;
+        }*/
     }
 
     public void onClick(View view) {
